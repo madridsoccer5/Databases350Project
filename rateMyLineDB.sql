7fe53a1471daaef2588eadaa7b203f80f0e1012a
grafpaper <grafpaper10@gmail.com>
2013-02-24 15:04:58 -0500

    Update rateMyLineDB.sql
    Added post table

--- rateMyLineDB.sql
+++ rateMyLineDB.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 `password` varchar(20) NOT NULL,
 PRIMARY KEY (`id`)
 )
+
+CREATE TABLE IF NOT EXISTS `posts` (
+`id` int(5) NOT NULL AUTO_INCREMENT,
+`username` varchar(20) NOT NULL,
+`date` date NOT NULL,
+`likes` int(3) NOT NULL,
+`dislikes` int(3) NOT NULL,
+PRIMARY KEY (`id`)
+)
