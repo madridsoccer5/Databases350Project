89b4349bc788687dcebdb579a3d650ed63c42372
unknown <grafpaper10@gmail.com>
2013-02-24 14:53:51 -0500

    Grafpaper added dbconnect.sql and modd of .sql

--- rateMyLineDB.sql
+++ rateMyLineDB.sql
@@ -3,9 +3,8 @@ GRANT ALL PRIVILEGES ON rmlDB.* to 'assist'@'localhost' identified by 'assist';
 USE rmlDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
-	`id` int(5) NOT NULL AUTO_INCREMENT,
-	`username` varchar(20) NOT NULL,
-	`password` varchar(20) NOT NULL,
-	PRIMARY KEY  (`id`)
+`id` int(5) NOT NULL AUTO_INCREMENT,
+`username` varchar(20) NOT NULL,
+`password` varchar(20) NOT NULL,
+PRIMARY KEY (`id`)
 )
-
