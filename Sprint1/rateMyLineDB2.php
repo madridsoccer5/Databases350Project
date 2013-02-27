71b87679742cfd078030675831efd13c8e801bd4
tsangbou <tsangbou@mail.umw.edu>
2013-02-26 21:25:02 -0500

    more updates

--- Sprint1/rateMyLineDB2.php
+++ Sprint1/rateMyLineDB2.php
@@ -46,8 +46,8 @@
     include('dbconnect.php');
     $date = $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
 					
-    $query = "INSERT INTO posts (username, date, post) VALUES ( '";
-    $query = $query . $userName . "', '" . $date . "', '" . $post . "')";
+    $query = "INSERT INTO posts (username, date, post, title) VALUES ( '";
+    $query = $query . $userName . "', '" . $date . "', '" . $post . "', '" . $title . "')";
 					echo "<p>QUERY   $query</p>";
 	$result = mysqli_query($db, $query)
     or die("Error Querying Database");
