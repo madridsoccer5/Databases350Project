4bdc4ce2a6cde296b94b44bf59c8a2f150c83f1c
Alex Lindeman <alex@ael.me>
2013-02-28 11:49:05 -0500

    added login system

---
+++ Sprint1/header.php
@@ -0,0 +1,34 @@
+
+<div class="header">
+	<div class="outernav">
+		<div class="nav">
+			<div class="innernav">
+				<ul>
+				
+					<!-- MENU -->
+					<li><a href="rateMyLineDBindex.php">Home</a></li>
+					<li><a href="rateMyLineDB.php">Add A Pickup Line</a></li>
+					<li><a href="rateMyLineDBlist.php">See List of Pick Up Lines</a></li>
+					<li><a href="rateMyLineDBlogin.php"><?php echo (@$_SESSION['loggedin']) ? 'Logout' : 'Login' ?></a></li>
+					<li><a href="rateMyLineDDtopcomments.php">Top Pickup Lines</a></li>
+					<li><a href="rateMyLineDBworstcomments.php">Worst Pickup Lines</a></li>
+					<!-- END MENU -->
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+		
+	<div class="clear"></div>
+	
+	<div class="title">
+		<div class="innertitle">
+			
+			<!-- TITLE -->
+			<h1><a href="#">Rate My Line!</a></h1>
+			<h2>Find a girl in no time!</h2>
+			<!-- END TITLE -->
+			
+		</div>
+	</div>
+</div>
--- Sprint1/rateMyLineDBlogin.php
+++ Sprint1/rateMyLineDBlogin.php
@@ -1,3 +1,45 @@
+<?php
+include "dbconnect.php";
+session_start();
+
+if (isset ($_SESSION['loggedin']))
+{
+	session_destroy();
+	die ('Logged out.<br /><a href="rateMyLineDBindex.php">Click here to go to the home page</a>');
+}
+
+if (isset ($_POST))
+{
+	if (@$_POST['login'] == 'login')
+	{
+		$name = $_POST['username'];
+		
+		// password in db is stored as sha1 hash
+		$password = sha1 ($_POST['password']);
+
+		$query = "SELECT * FROM users WHERE username = '$name'";
+		$result = mysqli_query($db, $query) or die("Error Querying Database");
+		
+		if ($row = mysqli_fetch_array($result))
+		{
+			if ($row['password'] == $password)
+			{
+				$_SESSION['loggedin'] = true;
+				header ('Location: rateMyLineDBindex.php');
+				exit;
+			}
+			else
+			{
+				$error[] = 'wrong password';
+			}
+		}
+		else
+		{
+			$error[] = 'that user does not exist';
+		}
+	}
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -9,7 +51,7 @@
 </head>
 <body>
 <?php
-   include('header.html');
+   include('header.php');
    
 ?>
 
@@ -19,70 +61,25 @@
 		<div class="innerpagewrapper">
 			<div class="page">
 				<div class="content">
-				<?
-	 include "dbconnect.php";
-  	   if (isset($_POST['username'])){
-  	     $name = $_POST['username'];
-         $password = $_POST['password'];
-
-         $query = "select * from users WHERE username = '$name' AND password = '$password'";
-         $result = mysqli_query($db, $query)
-         or die("Error Querying Database");
-         if ($row = mysqli_fetch_array($result))
-         {
-   		#echo $query;
-
-       }
-       }
-       
-				
-									</table>
-					<!-- END CONTENT -->
-<?php
-    include('dbconnect.php');
-    $date = $_POST['year'].'-'.$_POST['month'].'-'.$_POST['day'];
-					
-    $query = "INSERT INTO post (username, date, title, post) VALUES ( '$username', '$date', '$title',  '$post');";
-    $result = mysqli_query($db, $query)
-    or die("Error Querying Database");
-				
-				    ?>	
-					
-					</form>
-					<!-- END CONTENT -->
-					
-				</div>
-				
-				<?php
-				    include('SIDEnFOOTER.html');
-				?>
-				   
-          <?
-           php
-        if (isset($_POST['username'])) {
-        echo "<h2>Incorrect Username/Password</h2>";
-        }
-        ?>
-        
-        <h1>Login</h1>
-          <form method="post" action="login.php">
+<?php if (isset ($error)) foreach ($error as $e) echo '<div class="error">' . $e . '</div>'; ?>
+        <h3>Login</h3>
+          <form method="post" action="rateMyLineDBlogin.php">
     <p>
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" size="40" </p>
     <p>
     <label for="password">Password:</label>
-    <input type="password" id="password" name="pw" size="40" /></p>
+    <input type="password" id="password" name="password" size="40" /></p>
 
-    <p><input type="submit" value="login" name="submit" /></p>
+    <p><input type="submit" value="login" name="login" /></p>
   </form>
   
   <p><a href="createAccount.php">Create Account</p>
-				
-								   ?>
 
 			</div>
 		</div>
 	</div>
+<?php include('SIDEnFOOTER.html'); ?>
 </div>
 </body>
 </html>
