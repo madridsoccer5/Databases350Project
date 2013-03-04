cde6eb653808eb6d81185d1bc1d0b2da236bd566
Sean Lewis <student@Jennifers-MacBook-Pro.local>
2013-03-04 14:47:13 -0500

    Fixed header file to align correctly

--- Sprint1/header.php
+++ Sprint1/header.php
@@ -9,9 +9,10 @@
 					<li><a href="rateMyLineDBindex.php">Home</a></li>
 					<li><a href="rateMyLineDB.php">Add A Pickup Line</a></li>
 					<li><a href="rateMyLineDBlist.php">See List of Pick Up Lines</a></li>
-					<li><a href="rateMyLineDBlogin.php"><?php echo (@$_SESSION['loggedin']) ? 'Logout' : 'Login' ?></a></li>
 					<li><a href="rateMyLineDDtopcomments.php">Top Pickup Lines</a></li>
 					<li><a href="rateMyLineDBworstcomments.php">Worst Pickup Lines</a></li>
+					<li><a href="rateMyLineDBlogin.php"><?php echo (@$_SESSION['loggedin']) ? 'Logout' : 'Login' ?></a></li>
+
 					<!-- END MENU -->
 					
 				</ul>
