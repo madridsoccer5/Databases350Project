9f865bf39e87074f3128b263842211af2fbaadcd
David Heller <davidheller@David-Hellers-MacBook-Pro.local>
2013-03-13 14:48:17 -0400

    removed folder

--- Sprint1/like_dislike/core/db/connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect('localhost', 'root', '') or die(mysql_error());
-mysql_select_db('rmlDB');
-?>
--- Sprint1/like_dislike/core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include 'db/connect.php';
-include 'func/articles.php';
-include 'func/like.php';
-
-?>
