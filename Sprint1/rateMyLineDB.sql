7dac08f3bc533c5fec79a2079c770e37d9582a3b
David Heller <davidheller@252.umw.dynamic.apogeenet.net>
2013-03-14 10:48:30 -0400

    password needs to be 40 varchar for sha1 to work

--- Sprint1/rateMyLineDB.sql
+++ Sprint1/rateMyLineDB.sql
@@ -5,7 +5,7 @@ USE rmlDB;
 CREATE TABLE IF NOT EXISTS `users` (
 `id` int(5) NOT NULL AUTO_INCREMENT,
 `username` varchar(20) NOT NULL,
-`password` varchar(20) NOT NULL,
+`password` varchar(40) NOT NULL,
 PRIMARY KEY (`id`)
 );
 
