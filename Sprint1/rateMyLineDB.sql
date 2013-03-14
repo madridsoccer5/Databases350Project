930377078082656226e9f4fe7dcf0612c216c2b4
Sean Lewis <student@Jennifers-MacBook-Pro.local>
2013-03-14 11:49:59 -0400

    changed database tables

--- Sprint1/rateMyLineDB.sql
+++ Sprint1/rateMyLineDB.sql
@@ -12,17 +12,15 @@ PRIMARY KEY (`id`)
 CREATE TABLE IF NOT EXISTS `posts` (
 `id` int(5) NOT NULL AUTO_INCREMENT,
 `username` varchar(20) NOT NULL,
-`date` date NOT NULL,
 `post` blob(100000),
-`title`varchar(20) NOT NULL,
 `likes` int(3) NOT NULL,
 `dislikes` int(3) NOT NULL,
 PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS 'likes' (
-'like_id' int(11) NOT NULL AUTO_INCREMENT,
-'user_id' int(11) NOT NULL,
-'post_id' int(11) NOT NULL,
-PRIMARY KEY ('like_id')
+CREATE TABLE IF NOT EXISTS `likes` (
+`like_id` int(11) NOT NULL AUTO_INCREMENT,
+`user_id` int(11) NOT NULL,
+`post_id` int(11) NOT NULL,
+PRIMARY KEY (`like_id`)
 );
